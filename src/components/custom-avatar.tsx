5ae6c3a042d8301e25b0af708ae0cd8f68c7140d
ivandamien <ivandamieno99@gmail.com>
2024-02-04 16:31:30 +0300

    Graphql mutations and configs

---
+++ src/components/custom-avatar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Avatar as AntdAvatar, AvatarProps } from 'antd';
+type Props = AvatarProps & {
+    name: string;
+}
+
+const CustomAvatar = ({name, style, ...rest}: Props) => {
+  return (
+    <AntdAvatar
+          alt={'John Doe'}
+          size="small"
+          style={{
+              backgroundColor: '#87d068',
+              display: "flex",
+              alignItems: 'center',
+              border:'none'
+          }}>
+          {name}
+    </AntdAvatar>
+  );
+}
+
+export default CustomAvatar;
