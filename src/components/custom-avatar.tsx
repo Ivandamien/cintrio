a911ce6bdbf53fd9272141ca6434690f413423d5
ivandamien <ivandamieno99@gmail.com>
2024-02-05 12:59:22 +0300

    Building utility functions

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Avatar as AntdAvatar, AvatarProps } from 'antd';
+import { getNameInitials } from '@/utilities';
 type Props = AvatarProps & {
-    name: string;
+    name?: string;
 }
 
 const CustomAvatar = ({name, style, ...rest}: Props) => {
   return (
     <AntdAvatar
-          alt={'John Doe'}
+          alt={name}
           size="small"
           style={{
               backgroundColor: '#87d068',
               display: "flex",
               alignItems: 'center',
-              border:'none'
-          }}>
-          {name}
+              border: 'none',
+              ...style
+          }}
+      
+          {...rest}
+      >
+          {getNameInitials(name || "")}
     </AntdAvatar>
   );
 }
--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -2,7 +2,8 @@ import { Popover } from 'antd';
 import React from 'react';
 import CustomAvatar from '../custom-avatar';
 import { useGetIdentity } from '@refinedev/core';
-import type {User} from '@graphql/schema.types'
+import { User } from '@/graphql/schema.types';
+// import type {User} from '@graphql/schema.types'
 
 
 const CurrentUser = () => {
@@ -15,7 +16,12 @@ const CurrentUser = () => {
               overlayInnerStyle={{ padding: 0 }}
               overlayStyle={{zIndex:999}}
           >
-          <CustomAvatar  />    
+              <CustomAvatar
+                  name={user?.name}
+                  src={user?.avatarUrl}
+                  size={"default"}
+                  style={{cursor:"pointer"}}
+              />    
       </Popover>
       </>
       
