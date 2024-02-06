0aef3019a23c42b49672899976e5d17bbac0d126
ivandamien <ivandamieno99@gmail.com>
2024-02-06 12:08:41 +0300

    Building the dealchart

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import { DollarOutlined } from "@ant-design/icons";
+import { Card } from "antd";
+import React from "react";
+import { Text } from "../text";
 
 const DealsChart = () => {
   return (
-    <div>
-      
-    </div>
+    <Card
+      style={{ height: "100%" }}
+      title={
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+          }}
+        >
+          <DollarOutlined />
+          <Text size="sm" style={{ marginLeft: "0.5rem" }}>
+            Deals
+          </Text>
+        </div>
+      }
+    ></Card>
   );
-}
+};
 
 export default DealsChart;
