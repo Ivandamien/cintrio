974aa2944dd61879f2e0575ecef83216ac7f85b1
ivandamien <ivandamieno99@gmail.com>
2024-02-06 23:32:57 +0300

    Building the companies tables list

---
+++ src/pages/company/list.tsx
@@ -0,0 +1,32 @@
+import { CreateButton, List } from "@refinedev/antd";
+import { useGo } from "@refinedev/core";
+import { Table } from "antd";
+import React from "react";
+
+export const CompanyList = () => {
+  const go = useGo();
+
+  return (
+    <List
+      breadcrumb={false}
+      headerButtons={() => (
+        <CreateButton
+          onClick={() => {
+            go({
+              to: {
+                resource: "companies",
+                action: "create",
+              },
+              options: {
+                keepQuery: true,
+              },
+              type: "replace",
+            });
+          }}
+        />
+      )}
+    >
+      <Table></Table>
+    </List>
+  );
+};
