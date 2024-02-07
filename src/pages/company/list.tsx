b21ded0451109e92a77c31bf08a997cae6b0456c
ivandamien <ivandamieno99@gmail.com>
2024-02-07 16:38:21 +0300

    Building a filters and search functionalities

--- src/pages/company/list.tsx
+++ src/pages/company/list.tsx
@@ -1,16 +1,34 @@
 import React from "react";
-import { CreateButton, FilterDropdown, List, useTable } from "@refinedev/antd";
+import {
+  CreateButton,
+  DeleteButton,
+  EditButton,
+  FilterDropdown,
+  List,
+  useTable,
+} from "@refinedev/antd";
 import { getDefaultFilter, useGo } from "@refinedev/core";
 import { Input, Space, Table } from "antd";
 import { COMPANIES_LIST_QUERY } from "@/graphql/queries";
 import { SearchOutlined } from "@ant-design/icons";
 import CustomAvatar from "@/components/custom-avatar";
 import { Text } from "@/components/text";
+import { Company } from "@/graphql/schema.types";
+import { currencyNumber } from "@/utilities";
 
 export const CompanyList = () => {
   const go = useGo();
   const { tableProps, filters } = useTable({
     resource: "companies",
+    onSearch: (values) => {
+      return [
+        {
+          field: "name",
+          operator: "contains",
+          value: values.name,
+        },
+      ];
+    },
     pagination: {
       pageSize: 12,
     },
@@ -23,6 +41,23 @@ export const CompanyList = () => {
     //     order: "asc",
     //   },
     // ],
+    sorters: {
+      initial: [
+        {
+          field: "createdAt",
+          order: "desc",
+        },
+      ],
+    },
+    filters: {
+      initial: [
+        {
+          field: "name",
+          operator: "contains",
+          value: undefined,
+        },
+      ],
+    },
   });
 
   return (
@@ -53,7 +88,7 @@ export const CompanyList = () => {
           showTotal: (total) => `Total ${total} items`,
         }}
       >
-        <Table.Column
+        <Table.Column<Company>
           dataIndex="name"
           title="Company Title"
           defaultFilteredValue={getDefaultFilter("id", filters)}
@@ -71,6 +106,26 @@ export const CompanyList = () => {
                 src={record.avatarUrl}
               />
               <Text style={{ whiteSpace: "nowrap" }}>{record.name}</Text>
+            </Space>
+          )}
+        />
+        <Table.Column<Company>
+          dataIndex="totalRevenue"
+          title="Open deals amount"
+          render={(value, company) => (
+            <Text>
+              {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}
+            </Text>
+          )}
+        />
+        <Table.Column<Company>
+          dataIndex="id"
+          title="Actions"
+          fixed="right"
+          render={(value) => (
+            <Space>
+              <EditButton hideText size="small" recordItemId={value} />
+              <DeleteButton hideText size="small" recordItemId={value} />
             </Space>
           )}
         />
