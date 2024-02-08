036053d3ece62fca339d6aa48c0308bf1e9d3cc9
ivandamien <ivandamieno99@gmail.com>
2024-02-08 14:56:09 +0300

    Building the create modal

--- src/pages/company/create.tsx
+++ src/pages/company/create.tsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { CompanyList } from "./list";
+import { Form, Input, Modal } from "antd";
+import { useGo } from "@refinedev/core";
+import { useModalForm } from "@refinedev/antd";
+import { CREATE_COMPANY_MUTATION } from "@/graphql/mutations";
 
 const Create = () => {
-  return <div></div>;
+  const go = useGo();
+  const goToListPage = () => {
+    go({
+      to: { resource: "companies", action: "list" },
+      options: { keepQuery: true },
+      type: "replace",
+    });
+  };
+
+  const { formProps, modalProps } = useModalForm({
+    action: "create",
+    defaultVisible: true,
+    resource: "companies",
+    redirect: false,
+    mutationMode: "pessimistic",
+    onMutationSuccess: goToListPage,
+    meta: {
+      gqlMutation: CREATE_COMPANY_MUTATION,
+    },
+  });
+  return (
+    <CompanyList>
+      <Modal
+        {...modalProps}
+        mask={true}
+        onCancel={goToListPage}
+        title="create Company"
+        width={512}
+      >
+        <Form {...formProps} layout="vertical">
+          <Form.Item
+            label="Company name"
+            name="name"
+            rules={[{ required: true, message: "Company name is required" }]}
+          >
+            <Input placeholder="Please enter a company name" />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </CompanyList>
+  );
 };
 
 export default Create;
