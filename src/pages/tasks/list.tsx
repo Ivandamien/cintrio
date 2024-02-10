04d2dd541a721f680e601cdc923a07c9ef1aa693
ivandamien <ivandamieno99@gmail.com>
2024-02-10 09:56:19 +0300

    fetching and memomizing stages and tasks

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -3,14 +3,62 @@ import {
   KanbanBoardContainer,
 } from "@/components/tasks/kanban/board";
 import KanbanColumn from "@/components/tasks/kanban/column";
+import KanbanItem from "@/components/tasks/kanban/item";
+import { TASKS_QUERY } from "@/graphql/queries";
+import { useList } from "@refinedev/core";
 import React from "react";
 
 const List = () => {
+  const { data: stages, isLoading: isLoadingStages } = useList({
+    resource: "stages",
+    filters: [
+      {
+        field: 'title',
+        operator: 'in',
+        value:['TODO', 'IN PROGRESS', 'IN REVIEW', 'DONE]
+      }
+    ],
+    sorters: [
+      {
+        field: "createdAt",
+        order: "asc",
+      },
+    ],
+  });
+  const { data: tasks, isLoading: isLoadingTasks } = useList({
+    resource: "tasks",
+    sorters: [
+      {
+        field: "dueDate",
+        order: "asc",
+      },
+    ],
+    queryOptions: {
+      enabled:!!stages,
+    },
+    pagination: {
+      mode: "off",
+    },
+    meta: {
+      gqlQuery: TASKS_QUERY,
+    },
+  });
+
+  const taskStages = React.useMemo(() => {
+    if (!tasks?.data || !stages?.data) {
+      return {
+        unnasignedStage: [],
+        stages:[]
+      }
+    }
+  },[stages,tasks])
   return (
     <>
       <KanbanBoardContainer>
         <KanbanBoard>
-          <KanbanColumn></KanbanColumn>
+          <KanbanColumn>
+            <KanbanItem></KanbanItem>
+          </KanbanColumn>
         </KanbanBoard>
       </KanbanBoardContainer>
     </>
